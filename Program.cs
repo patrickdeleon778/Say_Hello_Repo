e6e04dba096ce84d2d2e93e6f7341e617a4cd272
patrickdeleon778_mac <patrickdeleon778@gmail.com>
2022-10-22 02:55:10 -0400

    Redid everything Say Hello

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("What is your name?");
+
+string? userInput = Console.ReadLine();
+
+Console.WriteLine($"Hello {userInput}.");
+
+//this is some random stuff that I wanted to add and clone and commit and stuff
